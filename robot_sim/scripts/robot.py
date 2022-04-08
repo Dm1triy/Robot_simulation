47a47233cafc818685b2ed5ab45ddfe64776087e
Dm1triy <52043491+Dm1triy@users.noreply.github.com>
2022-04-08 15:56:10 +0300

    Add files via upload

---
+++ robot_sim/scripts/robot.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from robot_sim.msg import Encoder
+
+
+
+def getNew(W1prev, W2prev, W1tar, W2tar):
+    t = 0.1
+    T = 0.01    ## чем больше Т тем быстрее происходит регуляция
+    betta = t/(t+T)
+    W1n = betta * W1prev + (1-betta)*W1tar
+    W2n = betta * W2prev + (1-betta)*W2tar
+    return (W1n, W2n)
+    
+def wheelVel(V, omega):
+    L = 0.287
+    r = 0.033
+    R = V/omega
+    l = R-L/2
+    Wleft = omega * l/r
+    l = R+L/2
+    Wright = omega * l/r
+    return Wleft, Wright
+	
+def velToEncoder (W1, W2):
+# кол-во оборотов = W(рад/с)*deltaТ(с)/2*pi, 
+# где deltaT - время между отправкой сообщений
+# если частота отправки сообщений 10Гц, то deltaT = 0.1(сек) 
+    n1 = W1*0.1/6.283
+    n2 = W2*0.1/6.283
+# кол-во оборотов * разрешение энкодеров на 1 оборот = кол-во импульсов энкодера
+    e1 = int(n1 * 4096)
+    e2 = int(n2 * 4096)
+    return (e1,e2)
+
+
+
+def sendToServ(w1_tar, w2_tar):
+    pub = rospy.Publisher('RobotToServer', Encoder, queue_size=10)
+
+    w1_prev = w2_prev = 0
+    message_id = 0
+    
+    now = rospy.Time.now()
+    rate = rospy.Rate(10)
+    while rospy.Time.now() < now + rospy.Duration.from_sec(20): ## 20 сек
+        w1_new, w2_new = getNew (w1_prev, w2_prev, w1_tar, w2_tar)
+        e1, e2 = velToEncoder (w1_new, w2_new)
+		
+        msg = Encoder()
+        msg.leftWheel = e1
+        msg.rightWheel = e2
+        
+        frame_id = f"Mesurment number {message_id}"
+
+        msg.header.stamp = rospy.Time.now()	
+        msg.header.seq = message_id	
+        msg.header.frame_id = frame_id
+        
+        try:		 
+            pub.publish(msg)
+        except rospy.ROSInterruptException:
+            continue
+            
+        rospy.loginfo(f"{frame_id}: Send to serv left {e1} and right {e2} Encoders.\n \
+Sending time: {msg.header.stamp}")
+	
+        message_id += 1
+        w1_prev, w2_prev = w1_new, w2_new
+        rate.sleep()
+
+	
+
+def robotCallback (data):
+    V = data.linear.x
+    omega = data.angular.z
+	
+    rospy.loginfo(rospy.get_caller_id() + 
+                  f'Get from serv V: {V} and omega {omega} %s' % rospy.Time.now())
+				  
+    w1_tar, w2_tar = wheelVel(V, omega)
+    
+    
+    sendToServ (w1_tar, w2_tar)
+    
+   
+    
+def listenServ ():
+    rospy.init_node('robot', anonymous = True)
+    rospy.Subscriber('ServerToRobot', Twist, robotCallback)
+    rospy.spin()
+
+
+if __name__=="__main__":
+    listenServ()
+	
+	
+	
---
+++ robot_sim/scripts/server.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import matplotlib.pyplot as plt
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose2D
+from robot_sim.msg import Encoder
+
+
+#W1 - left, W2 - right (рад/с)
+#V(m/s)
+#против часовой - положительное направление
+
+WheelsEncoder = []            ## e1, e2 (impulse_num)
+Moments = []                  ## time
+MessageNums = []              ## index
+WheelsVel = []                ## w1, w2 (rad/s)
+RobotVel = []                 ## V (m/s)
+RobotAngVel = []              ## omega (rad/s)
+RobotAng = [0]                ## theta (rad)
+Coordinates = [[0,0]]         ## x, y (m)
+
+
+
+def publishCoordinates(x, y, theta):
+    pub = rospy.Publisher('Debugging', Pose2D, queue_size = 10)
+    
+    pose = Pose2D()
+    pose.x = x
+    pose.y = y
+    pose.theta = theta
+    
+    pub.publish(pose)
+    
+    rospy.loginfo(rospy.get_caller_id() + 
+                  f' Send to debugging coordinates x = {x}(m), y = {y}(m),\
+theta = {theta}rad.\n Sending time: {rospy.Time.now()}') 
+
+
+def coordinates ():				# координаты 
+    dt = 0.1
+    temp = (RobotVel[-1]*np.cos(RobotAng[-1]) + RobotVel[-2]*np.cos(RobotAng[-2]))*dt/2
+    x = temp + Coordinates[-1][0]
+	
+    temp = (RobotVel[-1]*np.sin(RobotAng[-1]) + RobotVel[-2]*np.sin(RobotAng[-2]))*dt/2
+    y = temp + Coordinates[-1][1]
+    
+    theta = RobotAng[-1]
+    
+    publishCoordinates(x, y, theta)
+    	
+    Coordinates.append([x,y])
+
+
+def robotAng ():				# угол, на который повернут робот
+    dt = 0.1
+    temp = (RobotAngVel[-1] + RobotAngVel[-2])*dt/2
+    RobotAng.append(temp + RobotAng[-1])
+
+
+def robotAngVel ():				# реальная угловая скорость робота 
+    w1 = WheelsVel[-1][0]
+    w2 = WheelsVel[-1][1]
+    r = 0.033
+    L = 0.287
+    omega = r/L * (w2 - w1)
+    RobotAngVel.append(omega)
+
+
+def robotVel():    			    # реальная скорость робота с помощью угловых скоростей колес
+    w1 = WheelsVel[-1][0]
+    w2 = WheelsVel[-1][1]
+    r = 0.033	# meters
+    V = r/2 * (w1 + w2) #(m/s)
+    RobotVel.append(V)
+
+
+def encoderToAngVel(): 	# значение энкодера в угловую скорость колес
+## e = w*0.1*4096/6.28 => w = e * 6.28/(4096*0.1) 
+    e1 = WheelsEncoder[-1][0]
+    e2 = WheelsEncoder[-1][1]
+    dt = 0.1
+    w1 = e1 * 6.28/(4096 * dt)
+    w2 = e2 * 6.28/(4096 * dt)
+    WheelsVel.append([w1,w2])
+	
+	
+	
+def servCallback(data):
+    e1 = data.leftWheel
+    e2 = data.rightWheel
+    sending_time = data.header.stamp
+    message_index = data.header.seq
+    message_id = data.header.frame_id
+
+    rospy.loginfo(rospy.get_caller_id() + 
+                  f' {message_id}: Get from robot left:{e1}, right:{e2} Encoders.\n \
+Sending time:{sending_time}. Receiving time: {rospy.Time.now()}') 
+ 	 			  
+    WheelsEncoder.append([e1,e2])
+    Moments.append(sending_time)
+    MessageNums.append (message_index)
+	
+    encoderToAngVel()
+    robotVel()
+    robotAngVel()
+    if (len(Moments)>1):
+        robotAng()
+        coordinates()
+		
+		
+	
+def listenRobot():
+    rospy.Subscriber('RobotToServer', Encoder, servCallback)
+    rospy.spin()
+
+
+
+def sendToRobot(V, omega):
+    pub = rospy.Publisher('ServerToRobot', Twist, queue_size = 10)
+	
+    vel = Twist()
+    vel.linear.x = V
+    vel.angular.z = omega
+    
+    now = rospy.Time.now()
+    rate = rospy.Rate(10)
+    while rospy.Time.now() < now + rospy.Duration.from_sec(0.2):
+        pub.publish(vel)
+        rate.sleep()
+        
+    rospy.loginfo(f"send to robot V:{V}, omega: {omega} %s" % rospy.Time.now())
+    
+    
+    
+if __name__=="__main__":
+    rospy.init_node('server', anonymous=True)
+	
+    sendToRobot (0.5, 0.5)  ## Таргетные значения линейной и угловой скоростей робота
+    listenRobot()
+    
+    x = [i[0] for i in Coordinates]
+    y = [i[1] for i in Coordinates]
+    
+    plt.plot(x, y)
+    plt.show()
+	
+	
+	
